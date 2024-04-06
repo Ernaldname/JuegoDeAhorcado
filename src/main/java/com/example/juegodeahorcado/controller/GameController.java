c808e17df375d440ae87eaad5b2cdf46d0a295ae
Edinson Ramirez <edinsonr547@gmail.com>
2024-04-06 06:28:33 -0500

    Faltan 3 funciones y queda

--- src/main/java/com/example/juegodeahorcado/controller/GameController.java
+++ src/main/java/com/example/juegodeahorcado/controller/GameController.java
@@ -1,6 +1,7 @@
 package com.example.juegodeahorcado.controller;
 
 import com.example.juegodeahorcado.model.Player;
+import com.example.juegodeahorcado.view.alert.AlertBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -8,11 +9,15 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import com.example.juegodeahorcado.model.Dice;
 
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 public class GameController {
     @FXML
     private AnchorPane anchorPane;
     @FXML
     private TextField campoDeTextoLetraClave;
+
     private Player player;
     private Dice dice1;
     private ImageView diceImageView1;
@@ -20,10 +25,12 @@ public class GameController {
     public void setPlayer(Player player) {
         this.player = player;
     }
-
+    private Dice dice; // Instancia de la clase Dice
+    public GameController() {
+        dice = new Dice(); // Crear una instancia de la clase Dice
+    }
     @FXML
-    public void onHandleButtonRollTheDice(ActionEvent event) {
-
+    public void onHandleButtonRollTheDice(ActionEvent event) throws IOException {
         if (diceImageView1 != null) {
             anchorPane.getChildren().remove(diceImageView1);
         }
@@ -37,38 +44,39 @@ public class GameController {
             // Traemos el texto gracias al metodo getText y lo asignamos a una variable que creamos con el nombre "letraSecretaText"
             String letraSecretaText = campoDeTextoLetraClave.getText();
 
+            if (validarPalabraClave(letraSecretaText)) {
+                System.out.println("");
+            } else {
+                String mensaje = "Por favor, ingresa únicamente palabras en el campo de texto.";
+                AlertBox.showMessage("Error", "Palabra Clave Inválida", mensaje);
+            }
             // Los resultados de letras los convertimos en arreglos de tipo char para que sean recorridos y comparados
-
             char[] arregloDeLetra = letraSecretaText.toCharArray();
             char[] arregloDeLaPalabra = palabraClave.toCharArray();
 
             for (char caracter : arregloDeLaPalabra) {
                 boolean letraEncontrada = false;
-
                 for (char letra : arregloDeLetra) {
                     if (caracter == letra) {
                         letraEncontrada = true;
                         break; // Salir del bucle interno si se encuentra la letra
                     }
-
                 }
                 if (letraEncontrada) {
-                    System.out.println("La letra '" + caracter + "' está en el arreglo.");
-
-                } else {
-                    System.out.println("La letra '" + caracter + "' no está en el arreglo.");
-                    if (diceImageView1 != null) {
-                        anchorPane.getChildren().remove(diceImageView1);
-                    }
-                    dice1 = new Dice();
-                    diceImageView1 = dice1.getDiceImage();
-                    anchorPane.getChildren().addAll(diceImageView1);
+                    String mensaje = "Por favor, ingresa únicamente palabras en el campo de texto.";
+                    AlertBox.showMessage("Error", "Palabra Clave Inválida", mensaje);
+                }
+                else {
+                   {System.out.println("No esta en el arreglo: ");
+                       dice.decrementarContador();
+                       break;}
                 }
             }
         }
     }
-
-
+    private boolean validarPalabraClave(String palabraClave) {
+        return Pattern.matches("\\b[a-zA-Z]+\\b", palabraClave);
+    }
     @FXML
     public void onHandleButtonRollTheAyuda(ActionEvent event) {
     }
--- src/main/java/com/example/juegodeahorcado/model/Dice.java
+++ src/main/java/com/example/juegodeahorcado/model/Dice.java
@@ -3,22 +3,26 @@ package com.example.juegodeahorcado.model;
 import com.example.juegodeahorcado.controller.GameController;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import com.example.juegodeahorcado.controller.GameController;
 
 public class Dice {
+    private int contadorErrores;
+
+    private int maxIntentos = 6; // Contador máximo de intentos permitidos
+    private int intentoActual = 0; // Número de intento actual
 
     private GameController gameController;
-
     private ImageView diceImage;
 
     public Dice() {
+        contadorErrores = 6;
         updateHangmanImage(); // Inicias con el máximo de intentos permitidos
     }
-
+    public void decrementarContador() {
+        contadorErrores++;
+        updateHangmanImage();
+    }
     public void updateHangmanImage() {// Método para actualizar la imagen basada en el contador de errores
         String PATH = "/com/example/juegodeahorcado/images/dices/";
-        int maxIntentos = 6; // Máximo de intentos permitidos
-        int intentoActual = 5; // Número de intento actual
 
         String imagenPath = PATH + "dice" + intentoActual + ".png";
         // Verifica que el intento actual no sea mayor al máximo permitido
@@ -30,7 +34,7 @@ public class Dice {
                         new Image(String.valueOf(getClass().getResource(imagenPath)))
                 );
         this.diceImage.setFitWidth(300);
-        this.diceImage.setFitHeight(320);
+        this.diceImage.setFitHeight(330);
     }
 
     public GameController getGameController(){ return gameController; }
@@ -38,6 +42,5 @@ public class Dice {
     public ImageView getDiceImage() {
         return diceImage;
     }
-
 
 }
